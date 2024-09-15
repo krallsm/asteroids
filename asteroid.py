aded7e0199770986f0100f5f5433c9fe0e8dde4e
Sean Krall <krallsm@gmail.com>
2024-09-15 10:55:38 -0500

    Refactor update method in asteroid.py to correctly update the position

--- asteroid.py
+++ asteroid.py
@@ -8,4 +8,4 @@ class Asteroid(CircleShape):
     def draw(self, screen):
         pygame.draw.circle(screen, (255, 255, 255), self.position, self.radius, 2)
     def update(self, dt):
-        self.position = self.velocity * dt
+        self.position += self.velocity * dt
